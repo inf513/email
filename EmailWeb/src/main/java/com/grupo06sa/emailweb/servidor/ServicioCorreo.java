d160b29e843ea27756038d7292631c28073db6f3
pauljj25 <paul.jesus.24042012@gmail.com>
2017-06-28 19:20:05 -0400

    Enviar respuesta del correo
    ya se puede enviar una respuesta de correo desde el servidor automaqicamente

--- EmailWeb/src/main/java/com/grupo06sa/emailweb/servidor/ServicioCorreo.java
+++ EmailWeb/src/main/java/com/grupo06sa/emailweb/servidor/ServicioCorreo.java
@@ -6,9 +6,13 @@
 
 package com.grupo06sa.emailweb.servidor;
 
+import com.grupo06sa.emailweb.negocios.Analizador;
+import com.grupo06sa.emailweb.negocios.Respuesta;
 import java.io.IOException;
 import java.util.Properties;
 import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 /**
  * 
  * @author Alex Limbert Yalusqui <limbertyalusqui@gmail.com>
@@ -16,11 +20,46 @@ import javax.mail.*;
 public class ServicioCorreo {
     private String usuario = "";
     private String password = "";
-    
+    private String mailReceptor="";
+    private String asunto ="";
+    private String cuerpo="";
+    private Analizador a=new Analizador();
     public ServicioCorreo(String usuario, String pass){
         this.usuario = usuario;
         this.password = pass;
     }
+      
+      public void SendMail(String mailReceptor, String asunto, String cuerpo) {
+		this.mailReceptor = mailReceptor;
+		this.asunto = asunto;
+		this.cuerpo = cuerpo;
+
+		Properties props = new Properties();
+		props.put("mail.smtp.user", usuario);
+		props.put("mail.smtp.password", password);
+		props.put("mail.smtp.host", "mail.ficct.uagrm.edu.bo");
+		props.put("mail.smtp.port", "25");		
+		try {
+			Authenticator auth = new autentificadorSMTP();
+			Session session = Session.getInstance(props, auth);
+			// session.setDebug(true);
+
+			MimeMessage msg = new MimeMessage(session);
+			msg.setText(cuerpo);
+			msg.setSubject(asunto);
+			msg.setFrom(new InternetAddress(usuario + "@mail.ficct.uagrm.edu.bo"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(mailReceptor));
+			Transport.send(msg);
+		} catch (Exception mex) {
+			mex.printStackTrace();
+		}
+
+	}
+      private class autentificadorSMTP extends javax.mail.Authenticator {
+		public PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(usuario,password);
+		}
+	}
     
     public void readEmail(){
         // Obtenemos las propiedades del sistema
@@ -51,7 +90,12 @@ public class ServicioCorreo {
                     Address dfrom = mensaje.getFrom()[0];
                     Object ob = mensaje.getContent();                
                     mensaje.setFlag(Flags.Flag.SEEN, true);
+                    System.out.println("pase por aqui ---------->");
+                    // si el correo no fue leido se enviara una respuesta
+                    respuesta(mensaje.getSubject(), mensaje.getContent().toString(), mensaje.getFrom()[0].toString());
                     msg = new Mensaje("yo mismo", dfrom.toString(), mensaje.getSubject(), ob.toString());
+                   
+                   // respuesta(mensaje.getSubject(), mensaje.getContent().toString(), mensaje.getFrom()[0].toString());
                     System.out.println(msg.toString());
                 }                
             }
@@ -64,8 +108,16 @@ public class ServicioCorreo {
             System.err.println(ioe.toString());
         }
     }
+    public void respuesta(String sAsunto, String sMensaje, String sCorreo){
+       
+         Respuesta r=a.analizarTrama(sAsunto);
+         System.out.println(r.getCode() + "*****" + r.getDescripcion());
+         SendMail(sCorreo, "respuesta del servidor", r.getDescripcion());
+    
+    }
     public static void main(String[] main){
         ServicioCorreo s = new ServicioCorreo("grupo06sa", "grupo06grupo06");
-        s.readEmail();
+        //s.SendMail("pauljesus@hotmail.com", "es un mesaje atraves del servidor de ficct222222", "hola paul saludos del servidor de la uagrm");
+       s.readEmail();
     }
 }
